f46d38a47338a21ec7c29ed2aaacfeb156aa03db
Craig Cook <craig@craigs-mbp.home>
2023-04-04 12:39:46 +0100

    Adding install script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Catapult Dev Utils
+# About
+
+Catapult Dev Utils are handy scripts to help developers. They are mildly opionated and align with Catapult ways of working.
+
+
+## Installation
+
+    curl -o- https://raw.githubusercontent.com/catapultcx/cdu/master/install.sh | bash
+    
--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 { # Ensure the whole script has downloaded
 
+# Inspired by https://github.com/nvm-sh/nvm/blob/master/install.sh
 
 cdu_echo() {
   command printf %s\\n "$*" 2>/dev/null
@@ -25,14 +26,77 @@ cdu_install_dir() {
 
 cdu_source() {
   local CDU_GITHUB_REPO
-  CDU_GITHUB_REPO="${CDU_INSTALL_GITHUB_REPO:-catapult/cdu}"
+  CDU_GITHUB_REPO="${CDU_INSTALL_GITHUB_REPO:-catapultcx/cdu}"
   local CDU_VERSION
   CDU_VERSION="${CDU_INSTALL_VERSION:-$(cdu_latest_version)}"
   local CDU_SOURCE_URL
-  CDU_SOURCE_URL="https://github.com/${CDU_GITHUB_REPO}.git"
+#   CDU_SOURCE_URL="https://github.com/${CDU_GITHUB_REPO}.git"
+  CDU_SOURCE_URL="git@github.com:${CDU_GITHUB_REPO}.git"
   cdu_echo "$CDU_SOURCE_URL"
 }
 
+try_profile() {
+  if [ -z "${1-}" ] || [ ! -f "${1}" ]; then
+    return 1
+  fi
+  cdu_echo "${1}"
+}
+
+#
+# Detect profile file if not specified as environment variable
+# (eg: PROFILE=~/.myprofile)
+# The echo'ed path is guaranteed to be an existing file
+# Otherwise, an empty string is returned
+#
+detect_profile() {
+  if [ "${PROFILE-}" = '/dev/null' ]; then
+    # the user has specifically requested NOT to have cdu touch their profile
+    return
+  fi
+
+  if [ -n "${PROFILE}" ] && [ -f "${PROFILE}" ]; then
+    cdu_echo "${PROFILE}"
+    return
+  fi
+
+  local DETECTED_PROFILE
+  DETECTED_PROFILE=''
+
+  if [ "${SHELL#*bash}" != "$SHELL" ]; then
+    if [ -f "$HOME/.bashrc" ]; then
+      DETECTED_PROFILE="$HOME/.bashrc"
+    elif [ -f "$HOME/.bash_profile" ]; then
+      DETECTED_PROFILE="$HOME/.bash_profile"
+    fi
+  elif [ "${SHELL#*zsh}" != "$SHELL" ]; then
+    if [ -f "$HOME/.zshrc" ]; then
+      DETECTED_PROFILE="$HOME/.zshrc"
+    elif [ -f "$HOME/.zprofile" ]; then
+      DETECTED_PROFILE="$HOME/.zprofile"
+    fi
+  fi
+
+  if [ -z "$DETECTED_PROFILE" ]; then
+    for EACH_PROFILE in ".profile" ".bashrc" ".bash_profile" ".zprofile" ".zshrc"
+    do
+      if DETECTED_PROFILE="$(try_profile "${HOME}/${EACH_PROFILE}")"; then
+        break
+      fi
+    done
+  fi
+
+  if [ -n "$DETECTED_PROFILE" ]; then
+    cdu_echo "$DETECTED_PROFILE"
+  fi
+}
+
+
+uninstall_cdu() {
+  local INSTALL_DIR
+  INSTALL_DIR="$(cdu_install_dir)"
+  cdu_echo "$INSTALL_DIR"
+  rm -rf $INSTALL_DIR
+}
 
 install_cdu_from_git() {
   local INSTALL_DIR
@@ -72,8 +136,19 @@ install_cdu_from_git() {
 
 }
 
+cdu_do_install() {
+  install_cdu_from_git
+  cdu_echo 
+  cdu_echo "Now add the install dir to your path in $(detect_profile)"
+  cdu_reset
+}
 
-install_cdu_from_git
+cdu_reset() {
+  unset -f cdu_install_dir cdu_latest_version  \
+    cdu_source install_cdu_from_git  \
+    cdu_do_install cdu_reset 
+}
 
+cdu_do_install
 
 }
